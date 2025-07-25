7b93c9b6e23745769d3ef5c4e57cc031972ac0c7
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2025-07-25 21:22:21 +0545

    added .listen

--- server.ts
+++ server.ts
@@ -1,4 +1,9 @@
 import "./src/database/connection";
 import app from "./src/app";
+import { dotEnv } from "./src/config/config";
+
+app.listen(dotEnv.port, () => {
+  console.log(` Server is listening on port ${dotEnv.port}`);
+});
 
 export default app;
